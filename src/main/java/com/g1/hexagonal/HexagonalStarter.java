495751b7a1fb777b4016d5741f04db045db2ec4b
g1kwon <spring_shot@naver.com>
2022-10-16 10:44:32 +0900

    Initial Commit

---
+++ src/main/java/com/g1/hexagonal/HexagonalStarter.java
@@ -0,0 +1,13 @@
+package com.g1.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalStarter.class, args);
+	}
+
+}
---
+++ src/test/java/com/g1/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g1.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
